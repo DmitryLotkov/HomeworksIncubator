50c047527485c330ace5fb3f28241903ac001fc5
DmitryLotkov <dmitry.lotkov9@gmail.com>
2021-09-26 15:36:28 +0300

    HW(ADD NEW ERROR 404)

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 function Error404() {
     return (
         <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <img src="https://img.lovepik.com/element/40021/7866.png_860.png" alt="404ErrorImg"/>
+            <h1>Oops! Page not found</h1>
+
+            <div>Go back and try a different link</div>
         </div>
     )
 }
